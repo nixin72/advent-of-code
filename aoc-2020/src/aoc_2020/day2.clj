53761bb6be5fec5ebf15cafc6866badba211d029
Philip Dumaresq <phdumaresq@protonmail.com>
2020-12-03 21:44:43 -0500

    finished day 2, took way too long

--- aoc-2020/src/aoc_2020/day2.clj
+++ aoc-2020/src/aoc_2020/day2.clj
@@ -3,13 +3,19 @@
 (defn between [n lower upper]
   (and (>= n lower) (<= n upper)))
 
-;; wrong: 329
+(defn only-one [c1 c2 ct]
+  (and (or (= c1 ct) (= c2 ct))
+       (not= c1 c2)))
+
+(defn splitting [file]
+  (-> (slurp file)
+      (s/split #"\n")
+      ((fn [xs] (map (fn [x] (s/split x #":")) xs)))
+      ((fn [xs] (map (fn [x] [(s/split (x 0) #"\s") (x 1)]) xs)))
+      ((fn [xs] (map (fn [x] [[(s/split ((x 0) 0) #"-") ((x 0) 1)] (x 1)]) xs)))))
+
 (defn day-2-1 []
-  (let [lines (-> (slurp "./resources/day-2-input.txt")
-                  (s/split #"\n")
-                  ((fn [xs] (map (fn [x] (s/split x #":")) xs)))
-                  ((fn [xs] (map (fn [x] [(s/split (x 0) #"\s") (x 1)]) xs)))
-                  ((fn [xs] (map (fn [x] [[(s/split ((x 0) 0) #"-") ((x 0) 1)] (x 1)]) xs)))
+  (let [lines (-> (splitting "./resources/day-2-input.txt")
                   ((fn [xs] (map (fn [x] [(x 0) (re-seq (re-pattern ((x 0) 1)) (x 1))]) xs)))
                   ((fn [xs] (map (fn [x] [[(Integer/parseInt (((x 0) 0) 0))
                                            (Integer/parseInt (((x 0) 0) 1))] (x 1)]) xs)))
@@ -17,16 +23,13 @@
     (count lines)))
 
 (defn day-2-2 []
-  (let [lines (-> (slurp "./resources/day-2-input.txt")
-                  (s/split #"\n")
-                  ((fn [xs] (map (fn [x] (s/split x #":")) xs)))
-                  ((fn [xs] (map (fn [x] [(s/split (x 0) #"\s") (x 1)]) xs)))
-                  ((fn [xs] (map (fn [x] [[(s/split ((x 0) 0) #"-") ((x 0) 1)] (x 1)]) xs)))
-                  ((fn [xs] (map (fn [x] [[[(Integer/parseInt (((x 0) 0) 0))
-                                            (Integer/parseInt (((x 0) 0) 1))]
+  (let [lines (-> (splitting "./resources/day-2-input.txt")
+                  ((fn [xs] (map (fn [x] [[[(dec (Integer/parseInt (((x 0) 0) 0)))
+                                            (dec (Integer/parseInt (((x 0) 0) 1)))]
                                            (nth ((x 0) 1) 0)]
                                           (x 1)]) xs)))
-                  ((fn [xs] (filter (fn [x] (and (and (<= (+ 1 (((x 0) 0) 0)) (count (x 1)))
-                                                      (= (nth (x 1) (+ 1 (((x 0) 0) 0))) ((x 0) 1)))
-                                                 (= (nth (x 1) (+ 1 (((x 0) 0) 1))) ((x 0) 1)))) xs))))]
+                  ((fn [xs] (filter (fn [x] (only-one
+                                             (nth (x 1) (((x 0) 0) 0))
+                                             (nth (x 1) (((x 0) 0) 1))
+                                             ((x 0) 1))) xs))))]
     (count lines)))
